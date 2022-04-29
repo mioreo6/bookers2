2543887534e007cbcdf51724bd1dc1504cbe95cd
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-04-29 14:17:09 +0000

    [Error]4-25

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.hosts << "145e0da605b54284ab35c362a32f93a5.vfs.cloud9.us-east-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.active_job.queue_adapter = :inline
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
  
   root to: "homes#top"
+  get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :books, only: [:new, :index, :show]
   resources :users, only: [:show, :edit]
