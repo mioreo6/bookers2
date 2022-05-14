d16da119098bcdc927c2f230243b87601ef05d56
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-05-14 06:33:57 +0000

    [username]4-25

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
   has_one_attached :profile_image
-  
+
+  validates :name, uniqueness: true
 
   def get_profile_image(width, height)
     unless profile_image.attached?
